ec7fb2231503e4c848793c30236b83c0d3547e21
Kate Land <kateqland@gmail.com>
2022-11-26 22:15:19 -0800

    Update transaction.js

--- managemate/src/pages/transaction.js
+++ managemate/src/pages/transaction.js
@@ -5,6 +5,36 @@ import {useNavigate} from "react-router-dom";
 
 function Transaction() {
     const navigate = useNavigate();
+    const [roommateGroup, setRoommateGroup] = useState([]);
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        const token = window.localStorage.getItem("userkey");
+        console.log("token: ", token);
+        async function getRoommateGroup() {
+            try {
+                const res = await axios.get("/roommateGroup/", {headers: {'auth-token': token}});
+                console.log("roommateGroup data: ", res.data);
+                setRoommateGroup(res.data);
+            } catch (err) {
+                console.error(err.response.data);
+                alert("Not in a group.");
+            }
+        }
+        getRoommateGroup();
+
+        async function getTransactions() {
+            try {
+                const res = await axios.get("/finance/getTransactions", { gid: roommateGroup.gid }, {headers: {'auth-token': token}});
+                console.log("transaction data: ", res.data);
+                setTransactions(res.data);
+            } catch (err) {
+                console.error(err.response.data);
+                alert("Could not load transactions.");
+            }
+        }
+        getTransactions();
+      }, []);
 
   return (
     <div className='background'>
@@ -12,30 +42,37 @@ function Transaction() {
             <Button className='button' onClick={()=>navigate('/home')}>Home</Button>
         </div>
         <div className='row' style={{width: '80%', backgroundColor:'#FFFFFF', borderRadius:'20px', alignItems: 'center'}}>
-            <h3>First Last</h3>
-            <p style={{color: '#D33610'}}>You owe: -$124</p>
-            <p style={{color: '#1FD310'}}>You are owed: $148</p>
+            <h3>{roommateGroup.name}</h3>
         </div>
         <div className='column' style={{width: '100%', margin:'20px', justifyContent: 'center', alignItems: 'center'}}>
             <div className='row' style={{width: '80%', marginBottom:'20px', justifyContent: 'space-between', alignItems: 'center'}}>
                 <h1 style={{margin: '0px'}}>Transaction History</h1>
             </div>
             <div className='column' style={{width: '80%', backgroundColor:'#FFFFFF', borderRadius:'20px'}}>
+                {transactions.length === 0 && (
+                <div style={styles.transactionCard}>
+                    <div className='row'>
+                        <p style={{margin:'0px'}}>No transactions.</p>
+                    </div>
+                </div>
+                )}
+                {transactions.map((transaction, index) => (
                 <div style={styles.transactionCard}>
                     <div className='row' style={{marginBottom: '10px'}}>
-                        <div style={{display: 'flex', flexDirection: 'row', width: '80%'}}>
-                            <div style={styles.dateBox}>Oct 18</div>
+                        <div style={{display: 'flex', flexDirection: 'row', width: '100%'}}>
+                            <div style={styles.dateBox}>{transaction.date}</div>
                             <div>
-                                <h3 style={{margin:'0px'}}>Boba</h3>
-                                <p style={{marginBottom:'0px'}}>First Last paid ____</p>
+                                <h3 style={{margin:'0px'}}>{transaction.title}</h3>
+                                <p style={{marginBottom:'0px'}}>{"Borrower(s): " + transaction.borrowers}</p>
+                                <p style={{marginBottom:'0px'}}>Lender: {transaction.lender}</p>
+                                <p style={{marginBottom:'0px'}}>Amount: {transaction.amount}</p>
+                                
                             </div>
-                        </div>
-                        <div style={{marginLeft: 'auto'}}>
-                            <p>You borrowed ____</p>
                         </div>
                     </div>
                     <div className='bar'></div>
                 </div>
+                ))}
             </div>
         </div>
     </div>
@@ -57,7 +94,7 @@ const styles = {
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#5589D7",
-        width: '10%',
+        width: '20%',
         color: '#FFFFFF',
         fontSize: '18px',
         marginRight: '20px'

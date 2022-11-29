0c6e1f6b2c39bb83947af6577ed54a9451449ac4
Kate Land <kateqland@gmail.com>
2022-11-29 00:13:14 -0800

    getCalendar api call

--- managemate/src/components/events-utc.js
+++ managemate/src/components/events-utc.js
@@ -5,33 +5,35 @@ const baseData = [{
     "Description": "Sanchit Aggarwal",
     "Start": "2022-11-28T21:00:00.000Z",
     "End": "2022-11-28T22:00:00.000Z",
-    "RecurrenceRule": null,
-    "RecurrenceID": null,
-    "RecurrenceException": null,
-    "isAllDay": false
-  }, {
-    "TaskID": 5,
-    "OwnerID": 1,
-    "Title": "Take out the trash",
-    "Description": "Aritra Mullick",
-    "Start": "2022-11-29T23:00:00.000Z",
-    "End": "2022-11-29T23:10:00.000Z",
-    "RecurrenceRule": null,
-    "RecurrenceID": null,
-    "RecurrenceException": null,
-    "isAllDay": false
-  }, {
-    "TaskID": 6,
-    "OwnerID": 4,
-    "Title": "Create demo video",
-    "Description": "Kate Land",
-    "Start": "2022-11-28T18:00:00.000Z",
-    "End": "2022-11-28T20:00:00.000Z",
-    "RecurrenceRule": null,
-    "RecurrenceID": null,
-    "RecurrenceException": null,
-    "isAllDay": true
-  }];
+    // "RecurrenceRule": null,
+    // "RecurrenceID": null,
+    // "RecurrenceException": null,
+    // "isAllDay": false
+  }, 
+  // {
+  //   "TaskID": 5,
+  //   "OwnerID": 1,
+  //   "Title": "Take out the trash",
+  //   "Description": "Aritra Mullick",
+  //   "Start": "2022-11-29T23:00:00.000Z",
+  //   "End": "2022-11-29T23:10:00.000Z",
+  //   "RecurrenceRule": null,
+  //   "RecurrenceID": null,
+  //   "RecurrenceException": null,
+  //   "isAllDay": false
+  // }, {
+  //   "TaskID": 6,
+  //   "OwnerID": 4,
+  //   "Title": "Create demo video",
+  //   "Description": "Kate Land",
+  //   "Start": "2022-11-28T18:00:00.000Z",
+  //   "End": "2022-11-28T20:00:00.000Z",
+  //   "RecurrenceRule": null,
+  //   "RecurrenceID": null,
+  //   "RecurrenceException": null,
+  //   "isAllDay": true
+  // }];
+];
   export const customModelFields = {
     id: 'TaskID',
     title: 'Title',
@@ -51,32 +53,28 @@ const baseData = [{
   };
   
   const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
   
-  export const displayDate = new Date(Date.UTC(currentYear, 10, 29));
+              /*"name": "flying",
+              "start": "2022-11-29T19:00:17.000Z",
+              "end": "2022-11-29T20:00:17.000Z",
+              "associated_with": "638522c82ebe4ba37ae88afb",
+              "associated_with_name": "Abhishek Marda",
+              "gcal_event_id": "dk4r0t229aa624m38ftlj45hn4"*/
+  
+  export const displayDate = new Date();
   export const sampleData = baseData.map(dataItem => ({
     id: dataItem.TaskID,
     start: parseAdjust(dataItem.Start),
     end: parseAdjust(dataItem.End),
-    isAllDay: dataItem.isAllDay,
+    //isAllDay: dataItem.isAllDay,
     title: dataItem.Title,
     description: dataItem.Description,
-    recurrenceRule: dataItem.RecurrenceRule,
-    recurrenceId: dataItem.RecurrenceID,
-    recurrenceExceptions: dataItem.RecurrenceException,
+    // recurrenceRule: dataItem.RecurrenceRule,
+    // recurrenceId: dataItem.RecurrenceID,
+    // recurrenceExceptions: dataItem.RecurrenceException,
     ownerID: dataItem.OwnerID,
     personId: dataItem.OwnerID
-  }));
-  export const sampleDataWithResources = baseData.map(dataItem => ({
-    id: dataItem.TaskID,
-    start: parseAdjust(dataItem.Start),
-    end: parseAdjust(dataItem.End),
-    isAllDay: dataItem.isAllDay,
-    title: dataItem.Title,
-    description: dataItem.Description,
-    recurrenceRule: dataItem.RecurrenceRule,
-    recurrenceId: dataItem.RecurrenceID,
-    recurrenceExceptions: dataItem.RecurrenceException,
-    personId: randomInt(1, 4)
   }));
   export const sampleDataWithCustomSchema = baseData.map(dataItem => ({ ...dataItem,
     Start: parseAdjust(dataItem.Start),
--- managemate/src/pages/schedule.js
+++ managemate/src/pages/schedule.js
@@ -1,68 +1,89 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/general.css';
 import Button from 'react-bootstrap/Button';
-import { guid } from '@progress/kendo-react-common';
 import { Scheduler, DayView, WeekView } from '@progress/kendo-react-scheduler';
 import '@progress/kendo-date-math/tz/America/Los_Angeles';
-import { sampleDataWithCustomSchema, displayDate, customModelFields } from '../components/events-utc';
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 function Schedule() {
     const navigate = useNavigate();
-    const [view, setView] = React.useState('day');
-    const [date, setDate] = React.useState(displayDate);
-    const [data, setData] = React.useState(sampleDataWithCustomSchema);
+    const [view, setView] = useState('day');
+    const [date, setDate] = useState(new Date());
+    const [data, setData] = useState([]);
+    const [groupId, setGroupId] = useState(null);
+
+    const baseData = [
+      {
+          "name": "flying",
+          "start": "2022-11-29T19:00:17.000Z",
+          "end": "2022-11-29T20:00:17.000Z",
+          "associated_with": "638522c82ebe4ba37ae88afb",
+          "associated_with_name": "Abhishek Marda",
+          "gcal_event_id": "afisflh58pfi3i7ennke8a28ek"
+      }
+  ];
+
+    const customModelFields = {
+      id: 'gcal_event_id',
+      title: 'name',
+      description: 'associated_with_name',
+      start: 'start',
+      end: 'end',
+      // recurrenceRule: 'RecurrenceRule',
+      // recurrenceId: 'RecurrenceID',
+      // recurrenceExceptions: 'RecurrenceException'
+    };
+    useEffect(() => {
+      const token = window.localStorage.getItem("userkey");
+      console.log("token: ", token);
+      async function getGroupId() {
+          try {
+              const res = await axios.get("/user/checkUserInGroup/", {headers: {'auth-token': token}});
+              console.log("groupId: ", res.data.gid);
+              setGroupId(res.data.gid);
+          } catch (err) {
+              console.error(err.response.data);
+              console.log("Not in a group.");
+          }
+      }
+      getGroupId();
+    }, []);
+
+    useEffect(() => {
+      async function getCalendar() {
+          try {
+              const res = await axios.get("/calendar/" + groupId + "/getCalendar/");
+              console.log("calendar data: ", res.data);
+
+              //res.data.forEach()
+              //setData(baseData);
+              setData(res.data);
+          } catch (err) {
+              console.error(err.response.data);
+              console.log("Not in a group.");
+          }
+      }
+      getCalendar();
+  }, [groupId]);
+
+
     const handleViewChange = React.useCallback(event => {
       setView(event.value);
     }, [setView]);
     const handleDateChange = React.useCallback(event => {
       setDate(event.value);
     }, [setDate]);
-    const handleDataChange = React.useCallback(({
-      created,
-      updated,
-      deleted
-    }) => {
-      setData(old => old.filter(item => deleted.find(current => current.TaskID === item.TaskID) === undefined).map(item => updated.find(current => current.TaskID === item.TaskID) || item).concat(created.map(item => Object.assign({}, item, {
-        TaskID: guid()
-      }))));
-    }, [setData]);
 
   return (
     <div className='background'>
         <div style={{width: '90%', marginBottom: '20px'}}>
             <Button className='button' onClick={()=>navigate('/home')}>Home</Button>
         </div>
-        <Scheduler data={data} onDataChange={handleDataChange} view={view} 
+        <Scheduler data={data} 
+        view={view} 
         onViewChange={handleViewChange} 
         date={date} onDateChange={handleDateChange} modelFields={customModelFields} 
-        group={{
-          resources: [ 'Persons']
-        }} 
-        // resources={[{
-        //   name: 'Persons',
-        //   data: [{
-        //     text: 'roommate1',
-        //     value: 1,
-        //     color: '#73c2fb'
-        //   }, {
-        //     text: 'roommate2',
-        //     value: 2,
-        //     color: '#1e90ff'
-        //   }, {
-        //     text: 'roommate3',
-        //     value: 3,
-        //     color: '#1560bd'
-        //   }, {
-        //     text: 'roommate4',
-        //     value: 4,
-        //     color: '#1034a6'
-        //   }],
-        //   field: 'PersonIDs',
-        //   valueField: 'value',
-        //   textField: 'text',
-        //   colorField: 'color'
-        // }]}
         >
             <DayView />
             <WeekView />
